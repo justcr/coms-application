dd8161664ef2bcc4d30ab6f5a005a928280dc8a8
GuessWhatBBQ <saminslm@gmail.com>
2022-12-20 07:53:34 +0600

    fixed cors issue

--- server/server.js
+++ server/server.js
@@ -1,14 +1,23 @@
-const io = require('socket.io')(3000);
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 
-io.on('connection', socket => {
-  console.log('a user connected');
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
+io.on("connection", (socket) => {
+  console.log("a user connected");
 
-  socket.on('buttonClicked', data => {
-    console.log('received data:', data);
-    io.emit('buttonClicked', data);
+  socket.on("buttonClicked", (data) => {
+    console.log("received data:", data);
+    io.emit("buttonClicked", data);
   });
 
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
   });
 });
+
+httpServer.listen(3000);
